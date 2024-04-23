96f72481ba08a118439f4dc5b3a230cb74138a6f
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-04-23 15:38:59 +0200

    feat: Outlet and Link

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import {
 import Root from './routes/root'
 import ErrorPage from './error-page'
 
+import Contact from './routes/contact'
+
 import './index.css'
 
 const router = createBrowserRouter([
@@ -15,7 +17,14 @@ const router = createBrowserRouter([
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
-  }
+    children: [
+      {
+        path: '/contacts/:contactId',
+        element: <Contact />,
+      },
+    ]
+  },
+
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
